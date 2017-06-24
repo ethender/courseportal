cc6c949d4e88c661d6e5c94bc906905799b613de
ethender <ethender.9966526462@gmail.com>
2017-06-24 21:42:49 +0530

    starting project

---
+++ src/main/java/org/hawks/courseportal/config/WebApplicationContextConfig.java
@@ -0,0 +1,37 @@
+package org.hawks.courseportal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.hawks.courseportal")
+public class WebApplicationContextConfig extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+		configurer.enable();
+	}
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/").setCachePeriod(31556926);
+	}
+	
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
+	}
+}
